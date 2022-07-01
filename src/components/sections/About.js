5ad41a9d75e5ca8736641bac75ec38e46ae1526a
Alex <iordan.alexandru97@yahoo.com>
2022-07-01 13:18:15 +0200

    updated CV Link with my site

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -92,7 +92,7 @@ function About() {
                   <p>{aboutContent.content2}</p>
                   <p>{aboutContent.content3}</p>
                   <div className="mt-3">
-                    <a href="https://drive.google.com/uc?export=download&id=1F7NkRt5XCDHR91mMjYo-6P0WnYRYW78b" className="btn btn-default">
+                    <a href="https://drive.google.com/uc?export=download&id=1pQdr9NbrT-0DuXGdr1qciinfi4BPbzdj" className="btn btn-default">
                       Download CV
                     </a>
                   </div>
